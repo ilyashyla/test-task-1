2549f02ee9756c3be561e6efa4a8338c27c08aeb
Ilya Shyla <ilya.shyla@gmail.com>
2014-09-05 13:26:12 +0300

    no message

---
+++ js/main-page.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    popup();
+    popup_2();
+});
+function popup(){
+    var pageHeight = $(window).height();
+    var heigthOfPopup = $('.popup .popup-text').height() + 20;
+    var marginFromTop = ( pageHeight - heigthOfPopup ) / 2;
+    $('.popup .popup-text').css('marginTop',marginFromTop);
+}
+function popup_2(){
+    var pageHeight = $(window).height();
+    var heigthOfPopup = $('.popup-2 .popup-text-2').height() + 20;
+    var marginFromTop = ( pageHeight - heigthOfPopup ) / 2;
+    $('.popup-2 .popup-text-2').css('marginTop',marginFromTop);
+};
